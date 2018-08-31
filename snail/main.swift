5be8ab6ed63daa32f7be454f477ce7aa4299bcf4
apoc64 <sschwedt@gmail.com>
2018-08-31 09:08:23 -0600

    Initial commit, stubs out snail method and runs function with given parameters

--- snail/main.swift
+++ snail/main.swift
@@ -8,5 +8,18 @@
 
 import Foundation
 
-print("Hello, World!")
+//Given an n x n array, write a function that returns the array elements arranged from outermost elements to the middle element, traveling clockwise.
+let arrayMatrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
 
+func snail(matrix: [[Int]]) -> [Int] {
+    
+    
+    return [1, 2, 3]
+}
+
+print(snail(matrix: arrayMatrix))
+//#=> [1, 2, 3, 6, 9, 8, 7, 4, 5]
