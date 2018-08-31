690b819bfb549fe5ad16c88b5ca9f47294e1365c
apoc64 <sschwedt@gmail.com>
2018-08-31 09:04:38 -0600

    Initial Commit

---
+++ snail/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  snail
+//
+//  Created by Steven Schwedt on 8/31/18.
+//  Copyright © 2018 Steven Schwedt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
